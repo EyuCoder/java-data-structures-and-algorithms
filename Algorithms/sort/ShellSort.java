167cf3bb37debdbd12dd72648a10a3414d164fc0
elysium09 <eyueldaniel0921@gmail.com>
2021-08-27 17:40:09 +0300

    :fire: ShellSort.java

---
+++ Algorithms/sort/ShellSort.java
@@ -0,0 +1,29 @@
+/**
+* UnStable sort
+* time: O(n^2) Quadratic Time(could be better depending on the Gap)
+* in-place Algorithm
+*/
+public class ShellSort{
+
+	public static void main(String[] args) {
+		
+		int[] arr = {23, 435, 5,  6, 88, 99, -32, 4, -665};
+
+		for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+			
+			for(int i = gap; i < arr.length; i++){
+				int newElement = arr[i];
+
+				int j;
+				for(j = i; j >= gap && arr[j - gap] > newElement; j -= gap){
+					arr[j] = arr[j - gap];
+				}
+				arr[j] = newElement;
+			}
+		}
+
+		for(int item: arr){
+			System.out.println(item);	
+		}
+	}
+}
