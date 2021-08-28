3c96378624e466c1653d0c0ae4ebbd9333a2cecb
elysium09 <eyueldaniel0921@gmail.com>
2021-08-28 15:45:04 +0300

    :sparkles: QuickSort.java

---
+++ Algorithms/sort/QuickSort.java
@@ -0,0 +1,53 @@
+/**
+ * UnStable sort
+ * time: O(nlogn) loglinear complexity.
+ * In the worst case, it makes O(n2) comparisons, though this behavior is rare.
+ * in-place Algorithm
+ */
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void quickSort(int[] arr, int start, int end) {
+
+        if (start>=0&&end>=0) {
+            if (start< end) {
+                int pivot = partition(arr, start, end);
+
+                //Recursive function call
+                quickSort(arr, start, pivot - 1);
+                quickSort(arr, pivot + 1, end);          
+            }
+        }
+    }
+
+    public static int partition(int[] arr, int start, int end) {
+        //lomuto partition scheme
+        int pivot = end;
+        int i = start - 1;
+
+        for(int j = start; j <= end; j++){
+            if(arr[j] <= arr[pivot]){
+                swap(arr, ++i, j);
+            }
+        }
+
+        return i;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {23, 43, 43, 21, 2, 4, 77, 9, 0, 788, 7};
+        quickSort(arr, 0, arr.length - 1);
+
+        for (int item : arr) {
+            System.out.println(item);
+        }
+    }
+}
